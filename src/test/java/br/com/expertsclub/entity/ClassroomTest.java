a46769b599d2dba087007543da6e2a98d9f84464
Robson <robmartinhao@gmail.com>
2021-07-22 20:56:58 -0300

    Stream com lista de classes de estudantes

--- src/test/java/br/com/expertsclub/entity/ClassroomTest.java
+++ src/test/java/br/com/expertsclub/entity/ClassroomTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ClassroomTest {
 
@@ -30,7 +31,11 @@ public class ClassroomTest {
             names.add(student.getName());
         }
 
-        List<String> expected = Arrays.asList("Pedro Paulo", "Marcelo", "Ana", "Marcos", "Mateus", "Luana", "Pedro Henrique", "Monique", "Julia", "Marcia"); // Implemente aqui a solução utilizando o Stream API
+        List<String> expected = classrooms.stream()
+                .flatMap(classroom -> classroom.getStudents().stream())
+                .sorted(Comparator.comparing(Student::getAge))
+                .map(Student::getName)
+                .collect(Collectors.toList());
 
         System.out.println("Antes: " + names);
         System.out.println("Depois: " + expected);
@@ -51,7 +56,12 @@ public class ClassroomTest {
             }
         }
 
-        int expected = 39; // Implemente aqui a solução utilizando o Stream API
+        int expected = classrooms.stream()
+                .flatMap(classroom -> classroom.getStudents().stream())
+                .filter(student -> student.getName().startsWith("M"))
+                .filter(student -> student.getAge() >= 18)
+                .mapToInt(Student::getAge)
+                .sum();
 
         System.out.println("Antes: " + sum);
         System.out.println("Depois: " + expected);
